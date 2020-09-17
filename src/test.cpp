d84da9ed6853f97b5a9056ad16efd153f2127f66
Jerdan1980 <jeremytinana@gmail.com>
2020-09-17 17:07:26 -0400

    Display background

--- src/test.cpp
+++ src/test.cpp
@@ -1,24 +1,37 @@
 #include <SFML/Graphics.hpp>
+#include <string>
+#include "background.cpp"
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+int main() {
+  sf::RenderWindow window(sf::VideoMode(855,495), "SFML works!");
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	//create the background
+	const std::string bg = "0000000000000000000" +
+						(std::string)"0000012121212101110" +
+						(std::string)"0000021212121201110" +
+						(std::string)"0000012121212101110" +
+						(std::string)"0000021212121201110" +
+						(std::string)"0000012121212100000" +
+						(std::string)"0111021212121200000" +
+						(std::string)"0111012121212100000" +
+						(std::string)"0111021212121200000" +
+						(std::string)"0111012121212100000" +
+						(std::string)"0000000000000000000";
+	Background bgMap;
+	if (!bgMap.load("../resources/images/Board.png", sf::Vector2u(45, 45), bg, 19, 11))
+		return -1;	
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+			window.close();
+		}
 
-    return 0;
+		window.clear();
+		window.draw(bgMap);
+		window.display();
+	}
+
+	return 0;
 }
