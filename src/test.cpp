c22577c43071e0f3f514eb3586c5722b12b5747e
Jerdan1980 <jeremytinana@gmail.com>
2020-09-17 22:06:25 -0400

    Add piece display

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 #include "background.cpp"
+#include "boardmap.cpp"
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(855,495), "SFML works!");
@@ -20,6 +21,23 @@ int main() {
 	Background bgMap;
 	if (!bgMap.load("../resources/images/Board.png", sf::Vector2u(45, 45), bg, 19, 11))
 		return -1;	
+	
+	//get the pieces
+	const int board[] = 
+	{
+		+22, +24, +23, +21, +20, +23, +24, +22, +00,
+		+25, +25, +25, +25, +25, +25, +25, +25, +00,
+		+00, +00, +00, +00, +00, +00, +00, +00, +00,
+		+00, +00, +00, +00, +00, +00, +00, +00, +00,
+		+00, +00, +00, +00, +00, +00, +00, +00, +00,
+		+00, +00, +00, +00, +00, +00, +00, +00, +00,
+		+37, +37, +37, +37, +37, +37, +37, +37, +37,
+		+00, +32, +00, +00, +00, +00, +00, +31, +00,
+		+36, +35, +34, +33, +30, +33, +34, +35, +36
+	};
+	BoardMap boardMap;
+	if (!boardMap.load("../resources/images/Pieces.png", sf::Vector2u(45, 45), board, 9, 9, sf::Vector2u(5 * 45, 45)))
+		return -1;
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -30,6 +48,7 @@ int main() {
 
 		window.clear();
 		window.draw(bgMap);
+		window.draw(boardMap);
 		window.display();
 	}
 
