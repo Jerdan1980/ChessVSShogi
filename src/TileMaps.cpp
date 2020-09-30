f47bbf43d4ec1cdac232da7428ceb27a6a60c3ef
Jerdan1980 <jeremytinana@gmail.com>
2020-09-30 19:29:26 -0400

    Tilemap refactor part 1

--- src/TileMaps.cpp
+++ src/TileMaps.cpp
@@ -5,10 +5,18 @@
 
 class TiledMap : public sf::Drawable, public sf::Transformable {
 public:
-	bool load(const std::string& tileset, sf::Vector2u tileSize, const std::string& tiles, unsigned int width, unsigned int height, sf::Vector2u offset) {
+	bool load(const std::string& tileset, sf::Vector2u tileSize, const std::string& tiles, unsigned int width, unsigned int height, sf::Vector2i offset) {
 		//Load the tileset texture
 		if (!m_tileset.loadFromFile(tileset))
 			return false;
+
+		//save the vars
+		this->tileset = tileset;
+		this->tileSize = tileSize;
+		this->width = width;
+		this->height = height;
+		this->offset = offset;
+
 		
 		//resize the vertex array to fit the level size
 		m_vertices.setPrimitiveType(sf::Quads);
@@ -28,10 +36,10 @@ public:
 				sf::Vertex* quad = &m_vertices[(x + y * width) * 4];
 
 				//define its 4 corners
-				quad[0].position = sf::Vector2f(x * tileSize.x + offset.x, y * tileSize.y + offset.y);
-				quad[1].position = sf::Vector2f((x + 1) * tileSize.x + offset.x, y * tileSize.y + offset.y);
-				quad[2].position = sf::Vector2f((x + 1) * tileSize.x + offset.x, (y + 1) * tileSize.y + offset.y);
-				quad[3].position = sf::Vector2f(x * tileSize.x + offset.x, (y + 1) * tileSize.y + offset.y);
+				quad[0].position = sf::Vector2f((x + offset.x) * tileSize.x, (y + offset.y) * tileSize.y);
+				quad[1].position = sf::Vector2f((x + 1 + offset.x) * tileSize.x, (y + offset.y) * tileSize.y);
+				quad[2].position = sf::Vector2f((x + 1 + offset.x) * tileSize.x, (y + 1 + offset.y) * tileSize.y);
+				quad[3].position = sf::Vector2f((x + offset.x) * tileSize.x, (y + 1 + offset.y) * tileSize.y);
 
 				//define its 4 texture coordinates
 				quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
@@ -42,6 +50,37 @@ public:
 		}
 		return true;
 	}
+
+	bool update(const std::string& tiles) {
+		//populate the vertex array, with one quad per tile
+		for (unsigned int x = 0; x < width; x++) {
+			for (unsigned int y = 0; y < height; y++) {
+				//get the current tile number
+				int tileNumber = tiles[x + y * width] - '0';
+
+				//find its position in the tileset texture
+				int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+				int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+
+				//get a pointer to the current tile's quad
+				sf::Vertex* quad = &m_vertices[(x + y * width) * 4];
+
+				//define its 4 corners
+				quad[0].position = sf::Vector2f((x + offset.x) * tileSize.x, (y + offset.y) * tileSize.y);
+				quad[1].position = sf::Vector2f((x + 1 + offset.x) * tileSize.x, (y + offset.y) * tileSize.y);
+				quad[2].position = sf::Vector2f((x + 1 + offset.x) * tileSize.x, (y + 1 + offset.y) * tileSize.y);
+				quad[3].position = sf::Vector2f((x + offset.x) * tileSize.x, (y + 1 + offset.y) * tileSize.y);
+
+				//define its 4 texture coordinates
+				quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+				quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+				quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+				quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+			}
+		}
+		return true;
+	}
+
 private:
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
 		//apply the transform
@@ -54,16 +93,30 @@ private:
 		target.draw(m_vertices, states);
 	}
 
+	//variables
+	std::string tileset;
+	sf::Vector2u tileSize;
+	unsigned int width;
+	unsigned int height;
+	sf::Vector2i offset;
 	sf::VertexArray m_vertices;
 	sf::Texture m_tileset;
 };
 
 class PieceMap : public sf::Drawable, public sf::Transformable {
 public:
-	bool load(const std::string& tileset, sf::Vector2u tileSize, std::vector<Piece*> &pieces, unsigned int width, unsigned int height, sf::Vector2u offset) {
+	bool load(const std::string& tileset, sf::Vector2u tileSize, std::vector<Piece*> &pieces, unsigned int width, unsigned int height, sf::Vector2i offset) {
 		//load the tileset texture
 		if(!m_tileset.loadFromFile(tileset))
 			return false;
+		
+		//save the vars
+		this->tileset = tileset;
+		this->tileSize = tileSize;
+		this->width = width;
+		this->height = height;
+		this->offset = offset;
+
 		
 		//resize the vertex array to fit the level size
 		m_vertices.setPrimitiveType(sf::Quads);
@@ -83,10 +136,10 @@ public:
 				sf::Vertex* quad = &m_vertices[(x + y * width) * 4];
 				
 				//define its 4 corners
-				quad[0].position = sf::Vector2f(x * tileSize.x + offset.x, y * tileSize.y + offset.y);
-				quad[1].position = sf::Vector2f((x + 1) * tileSize.x + offset.x, y * tileSize.y + offset.y);
-				quad[2].position = sf::Vector2f((x + 1) * tileSize.x + offset.x, (y + 1) * tileSize.y + offset.y);
-				quad[3].position = sf::Vector2f(x * tileSize.x + offset.x, (y + 1) * tileSize.y + offset.y);
+				quad[0].position = sf::Vector2f((x + offset.x) * tileSize.x, (y + offset.y) * tileSize.y);
+				quad[1].position = sf::Vector2f((x + 1 + offset.x) * tileSize.x, (y + offset.y) * tileSize.y);
+				quad[2].position = sf::Vector2f((x + 1 + offset.x) * tileSize.x, (y + 1 + offset.y) * tileSize.y);
+				quad[3].position = sf::Vector2f((x + offset.x) * tileSize.x, (y + 1 + offset.y) * tileSize.y);
 
 				//define its 4 texture coords
 				bool flip = false; //tiles[x + y * width] < 0;
@@ -98,6 +151,36 @@ public:
 		}
 		return 1;
 	}
+	bool update(const std::vector<Piece*> &pieces) {
+		//populate the vertex array, with one quad per tile
+		for (unsigned int x = 0; x < width; x++) {
+			for (unsigned int y = 0; y < height; y++) {
+				//get the current tile number
+				int tileNumber = pieces[x + y * width]->getTilemap();
+
+				//find its position in the tileset texture
+				int tx = tileNumber % 10;
+				int ty = tileNumber / 10;
+
+				//get a pointer to the current tile's quad
+				sf::Vertex* quad = &m_vertices[(x + y * width) * 4];
+				
+				//define its 4 corners
+				quad[0].position = sf::Vector2f((x + offset.x) * tileSize.x, (y + offset.y) * tileSize.y);
+				quad[1].position = sf::Vector2f((x + 1 + offset.x) * tileSize.x, (y + offset.y) * tileSize.y);
+				quad[2].position = sf::Vector2f((x + 1 + offset.x) * tileSize.x, (y + 1 + offset.y) * tileSize.y);
+				quad[3].position = sf::Vector2f((x + offset.x) * tileSize.x, (y + 1 + offset.y) * tileSize.y);
+
+				//define its 4 texture coords
+				bool flip = false; //tiles[x + y * width] < 0;
+				quad[flip ? 2 : 0].texCoords = sf::Vector2f(tx * tileSize.x, ty * tileSize.y);
+				quad[flip ? 3 : 1].texCoords = sf::Vector2f((tx + 1) * tileSize.x, ty * tileSize.y);
+				quad[flip ? 0 : 2].texCoords = sf::Vector2f((tx + 1) * tileSize.x, (ty + 1) * tileSize.y);
+				quad[flip ? 1 : 3].texCoords = sf::Vector2f(tx * tileSize.x, (ty + 1) * tileSize.y);
+			}
+		}
+		return true;
+	}
 private:
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
 		//apply the transform
@@ -108,6 +191,12 @@ private:
 		target.draw(m_vertices, states);
 	}
 
+	//variables
+	std::string tileset;
+	sf::Vector2u tileSize;
+	unsigned int width;
+	unsigned int height;
+	sf::Vector2i offset;
 	sf::VertexArray m_vertices;
 	sf::Texture m_tileset;
 };
--- src/test.cpp
+++ src/test.cpp
@@ -7,13 +7,14 @@
 #include "BoardConstructor.cpp"
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode(855,495), "SFML works!");
+  sf::RenderWindow window(sf::VideoMode(19 * 45, 12 * 45), "SFML works!");
 	
 	//set a bool to redraw
 	bool redraw = true;
 
 	//create the background
-	const std::string bg = "0000000000000000000" +
+	const std::string bg = "2222222222222222222" +
+						(std::string)"0000000000000000000" +
 						(std::string)"0000012121212101110" +
 						(std::string)"0000021212121201110" +
 						(std::string)"0000012121212101110" +
@@ -25,7 +26,7 @@ int main() {
 						(std::string)"0111012121212100000" +
 						(std::string)"0000000000000000000";
 	TiledMap bgMap;
-	if (!bgMap.load("../resources/images/Board.png", sf::Vector2u(45, 45), bg, 19, 11, sf::Vector2u(0,0)))
+	if (!bgMap.load("../resources/images/Board.png", sf::Vector2u(45, 45), bg, 19, 12, sf::Vector2i(0,0)))
 		return -1;	
 	
 	//get the pieces
@@ -33,21 +34,22 @@ int main() {
 	std::vector<Piece*> pieces;
 	createBoard(pieces);
 	PieceMap boardMap;
-	if (!boardMap.load("../resources/images/Pieces.png", sf::Vector2u(45, 45), pieces, 9, 9, sf::Vector2u(5 * 45, 45)))
+	if (!boardMap.load("../resources/images/Pieces.png", sf::Vector2u(45, 45), pieces, 9, 9, sf::Vector2i(5, 2)))
 		return -1;
 
 	//highlighting
-	std::string high = "00000000000000000" +
-				(std::string)"00000000000200000" +
-				(std::string)"00000000001000000" +
-				(std::string)"00000000010000000" +
-				(std::string)"00000000100000000" +
-				(std::string)"00000001000000000" +
-				(std::string)"00000010000000000" +
-				(std::string)"00000100000000000" +
-				(std::string)"00000000000000000";
+	std::string high = "0000000000000000000" +
+				(std::string)"0000000000000000000" +
+				(std::string)"0000000000002000000" +
+				(std::string)"0000000000010000000" +
+				(std::string)"0000000000100000000" +
+				(std::string)"0000000001000000000" +
+				(std::string)"0000000010000000000" +
+				(std::string)"0000000100000000000" +
+				(std::string)"0000001000000000000" +
+				(std::string)"0000000000000000000";
 	TiledMap highMap;
-	if (!highMap.load("../resources/images/Highlights.png", sf::Vector2u(45, 45), high, 17, 9, sf::Vector2u(45, 45)))
+	if (!highMap.load("../resources/images/Highlights.png", sf::Vector2u(45, 45), high, 19, 10, sf::Vector2i(0, 1)))
 		return -1;	
 
 	while (window.isOpen()) {
@@ -65,9 +67,9 @@ int main() {
 						//get mouse position
 						sf::Vector2i localPosition = sf::Mouse::getPosition(window);
 						//get corresponding tile
-						sf::Vector2i tile = (localPosition - sf::Vector2i(45, 45)) / 45;
+						sf::Vector2i tile = (localPosition - sf::Vector2i(0, 45)) / 45;
 						//mock update (toggle between modes)
-						high[tile.x + tile.y * 17] = "012"[((high[tile.x + tile.y * 17] - '0') + 1) % 3];
+						high[tile.x + tile.y * 19] = "012"[((high[tile.x + tile.y * 19] - '0') + 1) % 3];
 						//slate tilemaps to be updated
 						redraw = true;
 					}
@@ -77,8 +79,8 @@ int main() {
 
 		//update tilemaps
 		if (redraw) {
-			boardMap.load("../resources/images/Pieces.png", sf::Vector2u(45, 45), pieces, 9, 9, sf::Vector2u(5 * 45, 45));
-			highMap.load("../resources/images/Highlights.png", sf::Vector2u(45, 45), high, 17, 9, sf::Vector2u(45, 45));
+			boardMap.update(pieces);
+			highMap.update(high);
 			redraw = false;
 		}
 
