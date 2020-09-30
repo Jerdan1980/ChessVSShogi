ead452e896ac9f9306d7f7b8cde3b61c8c82293a
Jerdan1980 <jeremytinana@gmail.com>
2020-09-30 19:39:13 -0400

    Add a header file for tilemap (refactor part 2)

---
+++ src/Tilemaps.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "Piece.h"
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <vector>
+
+class TiledMap : public sf::Drawable, public sf::Transformable {
+	public:
+		bool load(const std::string& tileset, sf::Vector2u tileSize, const std::string& tiles, unsigned int width, unsigned int height, sf::Vector2i offset);
+		bool update(const std::string& tiles);
+
+	private:
+		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+		//variables
+		std::string tileset;
+		sf::Vector2u tileSize;
+		unsigned int width;
+		unsigned int height;
+		sf::Vector2i offset;
+		sf::VertexArray m_vertices;
+		sf::Texture m_tileset;
+};
+
+class PieceMap : public sf::Drawable, public sf::Transformable {
+	public:
+		bool load(const std::string& tileset, sf::Vector2u tileSize, const std::vector<Piece*> &pieces, unsigned int width, unsigned int height, sf::Vector2i offset);
+		bool update(const std::vector<Piece*> &pieces);
+
+	private:
+		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+		//variables
+		std::string tileset;
+		sf::Vector2u tileSize;
+		unsigned int width;
+		unsigned int height;
+		sf::Vector2i offset;
+		sf::VertexArray m_vertices;
+		sf::Texture m_tileset;
+};
